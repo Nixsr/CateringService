82f019e9f150cb33bf796880597de7f7c8d69ed6
Antonio Anghelone <ant.anghelone@stud.uniroma3.it>
2022-06-03 15:12:15 +0200

    Aggiunte alcune modifiche al controller dello chef

--- src/main/java/it/uniroma3/siw/cateringService/controller/ChefController.java
+++ src/main/java/it/uniroma3/siw/cateringService/controller/ChefController.java
@@ -27,6 +27,8 @@ public class ChefController {
 	@Autowired
 	private ChefValidator validator;
 	
+	// Metodo POST per inserire un nuovo chef
+	
 	@PostMapping("/chef")
 	public String addBuffet(@Valid @ModelAttribute("chef") Chef chef, BindingResult bindingResult, Model model) {
 		validator.validate(chef, bindingResult);
@@ -38,18 +40,26 @@ public class ChefController {
 		return "chefForm.html";
 	}
 	
+	//Metodo GET per richiedere tutti gli chefs
+	
 	@GetMapping("/chef")
-	public String getBuffetList(Model model) {
+	public String getChefs(Model model) {
 		List<Chef> chef = chefService.findAll();
 		model.addAttribute("chefs", chef);
 		return "chefs.html";
 	}
+	
+	//Metodi GET per eliminare uno chef passando come riferimento l'id dello stesso
+	
+	//Mostra la conferma dell'eliminazione
 	
 	@GetMapping("/toDeleteChef/{id}")
 	public String toDeleteChef(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("chef", chefService.findById((id)));
 		return "toDeleteChef.html";
 	}
+	
+	//DELETE dello chef
 	
 	@GetMapping("/deleteChef/{id}")
 	public String deleteChef(@PathVariable("id") Long id, Model model) {
@@ -58,15 +68,17 @@ public class ChefController {
 		return "chefs.html";
 	}
 	
+	//Metodo GET per avere un singolo chef
+	
 	@GetMapping("/chef/{id}")
-	public String getBuffet(@PathVariable("id") Long id, Model model) {
+	public String getChef(@PathVariable("id") Long id, Model model) {
 		Chef chef = chefService.findById(id);
 		model.addAttribute("chef", chef);
 		return "chef.html";
 }
 	
 	@GetMapping("/chefForm")
-	public String getBuffet(Model model) {
+	public String getChef(Model model) {
 		model.addAttribute("chef", new Chef());
 		return "chefForm.html";
 	}
